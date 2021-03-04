abef3aab4ba0646cbfa7932e5cc6e25c683eb071
antoineeudes <antoine.eudes@gmail.com>
2021-03-04 17:54:03 +0100

    :sparkles: add object classification

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from errors import FailedToDownloadException, EmptyStringException, InvalidURL
-from utils import download_image
+from utils import download_image, predict_label
 
 
 st.title("Image classifier")
@@ -9,8 +9,14 @@ image_url = st.text_input("Image URL")
 if st.button("Classify!", key="enter"):
     try:
         img = download_image(image_url)
-        st.image(img)
+        st.image(img, width=200)
         error = ""
+        label = predict_label(img)
+        st.info(
+            "{object_type}: {confidence_rate}%".format(
+                object_type=label[1], confidence_rate=round(label[2] * 100, 2)
+            )
+        )
 
     except FailedToDownloadException:
         error = "Oops, something went wrong"
