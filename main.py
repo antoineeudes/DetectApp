1495b08c4e485c47a19395f5ceadf14473564745
antoineeudes <antoine.eudes@gmail.com>
2021-03-05 11:11:38 +0100

    :construction_worker: WIP

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import streamlit as st
 from errors import FailedToDownloadException, EmptyStringException, InvalidURL
-from utils import download_image, predict_label
+from utils import download_image, run_detector
 
 
 st.title("Image classifier")
 image_url = st.text_input("Image URL")
+error = ""
 
 if st.button("Classify!", key="enter"):
     try:
         img = download_image(image_url)
         st.image(img, width=200)
         error = ""
-        label = predict_label(img)
-        st.info(
-            "{object_type}: {confidence_rate}%".format(
-                object_type=label[1], confidence_rate=round(label[2] * 100, 2)
-            )
-        )
+        image_with_bounding_boxes = run_detector(img)
+        st.image(image_with_bounding_boxes, width=200)
 
     except FailedToDownloadException:
         error = "Oops, something went wrong"
